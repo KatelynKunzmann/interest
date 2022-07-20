337bd9de8fc5f564747045d0cf6779c544fcdf8a
Isaiah Gathala <92384661+isaiahgman@users.noreply.github.com>
2022-07-20 10:42:24 -0400

    refactoring

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
 import './App.css';
+import graduationCapImage from "./img/interesticon_mintgreen_cropped.png";
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 class="title">Welcome to Interest!
+        <figure>
+          <img src={graduationCapImage} width="100" 
+          alt="graduation cap with dollar sign in background"></img>
+        </figure>
+      </h1>
+
+      <h2>Student Loan Calculation and Visualization Tool</h2>
+
+      <h3>By: Katelyn Kunzmann, Pran Sutham, and Isaiah Guthala</h3>
+
+      <main>
+        <div id="description">
+          Have you ever stopped to think about how much in student loans you will need to take out 
+          or will have taken out by the time you are graduated?
+
+          <p>Well, our app will help you with just that!</p> 
+        </div> 
+
+      <section>
+        The only information you will need:
+        <div>
+          <ul>
+            <li>Example 1</li>
+            <li>Example 2</li>
+            <li>Example 3</li>
+            <li>Example 4</li>
+            <li>Example 5</li>
+            <li>Example 6</li>
+          </ul>
+        </div>
+      </section>
+
+      <div class="clicky-btn">
+        Get Started!
+      </div>
+
+      </main>
     </div>
   );
 }
-
 export default App;
