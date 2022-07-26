a04224bedbb8a375f0e8433ed4b4cfe577afce1c
Isaiah Gathala <92384661+isaiahgman@users.noreply.github.com>
2022-07-26 13:24:09 -0400

    form redesigned

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 import Home from './Pages/Home';
-import Form from './Pages/Form';
+import FormPage from './Pages/FormPage';
 import Verify from './Pages/Verify';
 import Visual from './Pages/Visual';
 import Error from './Pages/Error';
@@ -13,7 +13,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home/>} />
         <Route path="/interest" element={<Home/>} />
-        <Route path="/form" element={<Form/>} />
+        <Route path="/input" element={<FormPage/>} />
         <Route path="/verify" element={<Verify/>} />
         <Route path="/visual" element={<Visual/>} />
         <Route path="*" element={<Error/>} />
---
+++ src/components/Form.js
@@ -0,0 +1,69 @@
+import React from 'react'
+import { useForm } from "react-hook-form";
+import { useNavigate } from 'react-router-dom';
+
+function Form() {
+    const { register, handleSubmit } = useForm();
+    const navigate = useNavigate();
+
+    return (
+        <div className="container">
+        
+        <form onSubmit={handleSubmit((data) => {
+            navigate('/verify', {state: data});
+        })}>
+
+        <div className="row">
+            <div class="col-25">
+                <label>First Name</label>
+            </div>
+            <div class="col-75">
+                <input className="input" {...register("firstName", { required: true })} placeholder="enter your first name" />
+            </div>
+        </div>
+
+        <div className="row">
+            <div class="col-25">
+                <label>Last Name</label>
+            </div>
+            <div class="col-75">
+                <input className="input" {...register("lastName", { required: true })} placeholder="enter your last name" />
+            </div>
+        </div>
+
+        <div className="row">
+            <div class="col-25">
+                <label>Loan Amount</label>
+            </div>
+            <div class="col-75">
+                <input className="input" {...register("loanAmount", { required: true })} placeholder="enter the loan amount" />
+            </div>
+        </div>
+
+        <div className="row">
+            <div class="col-25">
+                <label>Loan Term</label>
+            </div>
+            <div class="col-75">
+                <input className="input" {...register("loanTerm", { required: true })} placeholder="enter the loan term" />
+            </div>
+        </div>
+
+        <div className="row">
+            <div class="col-25">
+                <label>Interest Rate</label>
+            </div>
+            <div class="col-75">
+                <input className="input" {...register("interestRate", { required: true })} placeholder="enter your loan's interest rate" />
+            </div>
+        </div>
+
+        <input className="clicky-btn" type="submit"/>
+
+        </form>
+
+        </div>
+    )
+}
+
+export default Form
